e6787df16436e893f834b38d269b2e659c7e88a6
course <joseclaris65@yahoo.comgit config --global user.name Jose>
2020-03-24 04:31:51 +0000

    Added custom user model.

--- app/core/migrations/tests/tests_models.py
+++ app/core/migrations/tests/tests_models.py
@@ -19,3 +19,12 @@ class ModelTests(TestCase):
         email = "joseclaris65@YAHOO.COM"
         user = get_user_model().objects.create_user(email, "test1234")
         self.assertEqual(user.email, email.lower())
+
+    def test_create_new_superuser(self):
+        "TEst creating a new superuser"
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
