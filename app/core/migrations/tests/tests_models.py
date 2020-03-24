7de168025127dda129116405801e57ee7c175420
course <joseclaris65@yahoo.comgit config --global user.name Jose>
2020-03-24 02:47:15 +0000

    Added Models for Users

---
+++ app/core/migrations/tests/tests_models.py
@@ -0,0 +1,21 @@
+#Test that model can create a user
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        #TEst creating a new user with an email is successful
+        email = "joseclaris65@yahoo.com"
+        password = "Joseis44"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        
+    def test_new_user_email_normaized(self):
+        email = "joseclaris65@YAHOO.COM"
+        user = get_user_model().objects.create_user(email, "test1234")
+        self.assertEqual(user.email, email.lower())
